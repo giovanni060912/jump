48866d4eea3431283fdd8a5d39c4dd8332a7b737
Giovanni Santos <giovannisantos060912@gmail.com>
2021-01-09 09:33:38 -0300

    create new project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import caderno from "./caderno.png";
+import { Icon } from "@iconify/react";
+import ellipsisVertical from "@iconify-icons/ion/ellipsis-vertical";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div className="App">
+        <div className="box1">
+          <h1>Default State</h1>
+          <img src={caderno} />
+          <h3>Corporate Bilaws</h3>
+          <p>
+            An internal document that details the <br /> operating rules for the
+            corporation and are <br /> typically adopted at the organizational{" "}
+            <br />
+            meeting of the board of directors.
+          </p>
+          <button className="view">View document</button>
+        </div>
+        <div className="space"></div>
+        <div className="box2">
+          <h1>Houver State</h1>
+          <ul>
+            <img src={caderno} />
+            <a>
+              <Icon icon={ellipsisVertical} />
+            </a>
+          </ul>
+          <h3>Corporate Bilaws</h3>
+          <p>
+            An internal document that details the <br /> operating rules for the
+            corporation and are <br /> typically adopted at the organizational{" "}
+            <br />
+            meeting of the board of directors.
+          </p>
+          <button className="view">View document</button>
+        </div>
+        <div className="but">
+          <button className="dow">Download</button>
+          <button className="dow">Print</button>
+          <button className="delete">Delete</button>
+        </div>
+      </div>
+    </body>
   );
 }
 
